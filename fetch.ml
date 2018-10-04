21f24595fbc32ecca0d8d572e0e9298f5d8829ca
Donatas Petrauskas <donatasp@wix.com>
2018-10-04 14:43:59 +0300

    Fetch testable code guide content.

--- fetch.ml
+++ fetch.ml
@@ -15,18 +15,23 @@ let rec tree_remove_attributes node =
   node |> children |> elements |> iter tree_remove_attributes
 
 let () =
-  let tbl = read_from_cache () in
-  let body = Lwt_main.run (fetch ~cache:tbl "http://misko.hevery.com/code-reviewers-guide/") in
-  write_to_cache tbl;
-  let open Soup in
-  let content = parse body $ "#content" in
-  let title =
-    children content
-    |> elements
-    |> filter (fun el -> name el = "h2")
-    |> first
-    |> Option.map ~f:to_string |> Option.value ~default:"?" in
-  let nodes = content $ ".entry" |> children |> elements |> to_list in
-  let () = List.iter ~f:tree_remove_attributes nodes in
-  print_endline ("title: " ^ title);
-  print_endline ("entry: " ^ (List.map ~f:to_string nodes |> String.concat ~sep:"\n"))
+  match Sys.argv with
+  | [|_; url|] ->
+    let tbl = read_from_cache () in
+    let body = Lwt_main.run (fetch ~cache:tbl url) in
+    write_to_cache tbl;
+    let open Soup in
+    let content = parse body $ "#content" in
+    let title =
+      children content
+      |> elements
+      |> filter (fun el -> name el = "h2")
+      |> first
+      |> Option.map ~f:to_string |> Option.value ~default:"?" in
+    let nodes = content $ ".entry" |> children |> elements |> to_list in
+    let () = List.iter ~f:tree_remove_attributes nodes in
+    print_endline ("title: " ^ title);
+    print_endline ("entry: " ^ (List.map ~f:to_string nodes |> String.concat ~sep:"\n"))
+
+  | _ ->
+    Out_channel.output_string stderr "pass URL as an argument.\n"
